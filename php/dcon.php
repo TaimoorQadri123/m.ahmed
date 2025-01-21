3c69cb5eb206bff684a4a3a78d09a1b820f38eb3
TaimoorQadri123 <dchota374@gmail.com>
2025-01-20 21:03:36 -0800

    role based not completed

--- php/dcon.php
+++ php/dcon.php
@@ -1,5 +1,5 @@
 <?php
-$server = 'mysql:host=localhost;port=3306;dbname=p2b_ecom';
+$server = 'mysql:host=localhost;port=3306;dbname=pr2_ecom';
 $user = 'root';
 $password = '';
 $pdo = new PDO($server,$user,$password)
